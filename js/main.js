1b81f82b1108d4d48525af00e4adabb229d217f7
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-06-27 10:16:09 +0200

    delete line

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const numbers = [32000, 31990, 32200, 32150, 31800, 32300, 32100, 32050, 31900, 32250, 31950, 32180, 32070];
+
 const menuBtn = document.querySelector('.hero__header-menu-icon')
 const menuDiv = document.querySelector('.hero__menu')
 
@@ -6,3 +8,4 @@ menuBtn.addEventListener('click', (e) => {
     menuDiv.classList.toggle('hero__menu_active')
     menuBtn.classList.toggle("hero__header-menu-icon_active")
 })
+
