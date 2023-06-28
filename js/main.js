6b70e72fbca25a89ac47f6df4ba64b8ae2f07fd5
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-06-28 17:39:25 +0200

    slider, query selector null

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-const numbers = [32000, 31990, 32200, 32150, 31800, 32300, 32100, 32050, 31900, 32250, 31950, 32180, 32070];
-
 const menuBtn = document.querySelector('.hero__header-menu-icon')
 const menuDiv = document.querySelector('.hero__menu')
 
@@ -9,3 +7,25 @@ menuBtn.addEventListener('click', (e) => {
     menuBtn.classList.toggle("hero__header-menu-icon_active")
 })
 
+const sliderImg = document.querySelector('.features__slider')
+
+let translateVal = 0
+
+const liArray = document.querySelectorAll('.features__controls li')
+liArray.forEach(function sliderDots(indicator, index) {
+    indicator.addEventListener('click', () => {
+        translateVal = index
+        document.querySelector('.features__controls-li.features__selected')?.classList.add('features__selected');
+
+        indicator.classList.add('features__selected')
+        sliderImg.style.transform = 'translate(' + translateVal * -20 + '%)'
+    })
+})
+
+/* setInterval(function sliderTimer() {
+    translateVal <= 4 ? translateVal * -20 : translateVal = 0
+    sliderImg.style.transform = 'translate(' + translateVal * -20 + '%)'
+
+    console.log(translateVal)
+}, 1000)
+ */
